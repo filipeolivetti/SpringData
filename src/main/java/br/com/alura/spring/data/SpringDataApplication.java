659f209242ee60967714a9ce0e9fb340654d636a
filipe <filipe.olivetti@poloar.com.br>
2020-11-30 15:47:41 -0300

    fim de curso

--- src/main/java/br/com/alura/spring/data/SpringDataApplication.java
+++ src/main/java/br/com/alura/spring/data/SpringDataApplication.java
@@ -5,36 +5,40 @@ import java.util.Scanner;
 import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
 
-import br.com.alura.spring.data.orm.Cargo;
-import br.com.alura.spring.data.repository.CargoRepository;
 import br.com.alura.spring.data.service.CrudCargoService;
 import br.com.alura.spring.data.service.CrudFuncionarioService;
 import br.com.alura.spring.data.service.CrudUnidadeTrabalhoService;
+import br.com.alura.spring.data.service.RelatorioFuncionarioDinamico;
 import br.com.alura.spring.data.service.RelatoriosService;
 
+@EnableJpaRepositories
 @SpringBootApplication
 public class SpringDataApplication implements CommandLineRunner {
-	
+
 	private Boolean system = true;
 
 	private final CrudCargoService cargoService;
 
+	private final RelatoriosService relatoriosService;
+	
 	private final CrudFuncionarioService funcionarioService;
 
 	private final CrudUnidadeTrabalhoService unidadeTrabalhoService;
-	
-	private final RelatoriosService relatoriosService;
 
-	public SpringDataApplication(CrudCargoService cargoService,
+	private final RelatorioFuncionarioDinamico relatorioFuncionarioDinamico;
+	
+	public SpringDataApplication(CrudCargoService cargoService, 
+			RelatoriosService relatoriosService, 
 			CrudFuncionarioService funcionarioService, 
 			CrudUnidadeTrabalhoService unidadeTrabalhoService,
-			RelatoriosService relatoriosService) {
-		
+			RelatorioFuncionarioDinamico relatorioFuncionarioDinamico) {
 		this.cargoService = cargoService;
+		this.relatoriosService = relatoriosService;
 		this.funcionarioService = funcionarioService;
 		this.unidadeTrabalhoService = unidadeTrabalhoService;
-		this.relatoriosService =  relatoriosService;
+		this.relatorioFuncionarioDinamico = relatorioFuncionarioDinamico;
 	}
 
 	public static void main(String[] args) {
@@ -48,25 +52,30 @@ public class SpringDataApplication implements CommandLineRunner {
 		while (system) {
 			System.out.println("Qual função deseja executar?");
 			System.out.println("0 - Sair");
-			System.out.println("1 - Funcionario");
-			System.out.println("2 - Cargo");
+			System.out.println("1 - Cargo");
+			System.out.println("2 - Funcionario");
 			System.out.println("3 - Unidade");
-			System.out.println("4 - Relatórios");
+			System.out.println("4 - Relatorios");
+			System.out.println("5 - Relatorio dinamico");
 			
+
 			Integer function = scanner.nextInt();
 
 			switch (function) {
 				case 1:
-					funcionarioService.inicial(scanner);
+					cargoService.inicial(scanner);
 					break;
 				case 2:
-					cargoService.inicial(scanner);
+					funcionarioService.inicial(scanner);
 					break;
 				case 3:
 					unidadeTrabalhoService.inicial(scanner);
 					break;
 				case 4:
 					relatoriosService.inicial(scanner);
+					break;
+				case 5:
+					relatorioFuncionarioDinamico.inicial(scanner);
 					break;
 				default:
 					System.out.println("Finalizando");
--- src/main/java/br/com/alura/spring/data/service/RelatoriosService.java
+++ src/main/java/br/com/alura/spring/data/service/RelatoriosService.java
@@ -8,29 +8,32 @@ import java.util.Scanner;
 import org.springframework.stereotype.Service;
 
 import br.com.alura.spring.data.orm.Funcionario;
+import br.com.alura.spring.data.orm.FuncionarioProjecao;
 import br.com.alura.spring.data.repository.FuncionarioRepository;
 
 @Service
 public class RelatoriosService {
-	
+
 	private Boolean system = true;
 	private final DateTimeFormatter formatter = DateTimeFormatter.ofPattern("dd/MM/yyyy");
-	private FuncionarioRepository funcionarioRepository;
 	
-
-	public RelatoriosService( FuncionarioRepository funcionarioRepository) {
+	private final FuncionarioRepository funcionarioRepository;
+	
+	public RelatoriosService(FuncionarioRepository funcionarioRepository) {
 		this.funcionarioRepository = funcionarioRepository;
 	}
-
+	
 	public void inicial(Scanner scanner) {
-		while (system) {
-			System.out.println("Qual ação tomar?");
+		while(system) {
+			System.out.println("Qual relatorio deseja executar");
 			System.out.println("0 - Sair");
-			System.out.println("1 - Buscar funcionario por nome");
-			System.out.println("2 - Buscar funcionario nome,data contratação e salario maior.");
-
+			System.out.println("1 - Busca funcionario nome");
+			System.out.println("2 - Busca funcionario nome, data contratacao e salario maior");
+			System.out.println("3 - Busca funcionario data contratacao");
+			System.out.println("4 - Pesquisa funcionario salario");
+			
 			int action = scanner.nextInt();
-
+			
 			switch (action) {
 			case 1:
 				buscaFuncionarioNome(scanner);
@@ -38,29 +41,56 @@ public class RelatoriosService {
 			case 2:
 				buscaFuncionarioNomeSalarioMaiorData(scanner);
 				break;
+			case 3:
+				buscaFuncionarioDataContratacao(scanner);
+				break;
+			case 4:
+				pesquisafuncionarioSalario();
+				break;
+			default:
+				system = false;
+				break;
 			}
+			
 		}
 	}
-
+	
 	private void buscaFuncionarioNome(Scanner scanner) {
-		System.out.println("Qual nome deseja pesquisar?");
+		System.out.println("Qual nome deseja pesquisar");
 		String nome = scanner.next();
 		List<Funcionario> list = funcionarioRepository.findByNome(nome);
-		list.forEach(System.out::println);	
+		list.forEach(System.out::println);
 	}
 	
 	private void buscaFuncionarioNomeSalarioMaiorData(Scanner scanner) {
-		System.out.println("Qual nome deseja pesquisar?");
+		System.out.println("Qual nome deseja pesquisar");
 		String nome = scanner.next();
 		
-		System.out.println("Qual data contratacao deseja pesquisar?");
+		System.out.println("Qual data contratacao deseja pesquisar");
 		String data = scanner.next();
 		LocalDate localDate = LocalDate.parse(data, formatter);
 		
-		System.out.println("Qual salario deseja pesquisar?");
+		System.out.println("Qual salario deseja pesquisar");
 		Double salario = scanner.nextDouble();
 		
-		List<Funcionario> list =funcionarioRepository.findNomeSalarioMaiorDataContratacao(nome, salario, localDate);
+		List<Funcionario> list = funcionarioRepository
+				.findNomeSalarioMaiorDataContratacao(nome, salario, localDate);
 		list.forEach(System.out::println);
 	}
+	
+	private void buscaFuncionarioDataContratacao(Scanner scanner) {
+		System.out.println("Qual data contratacao deseja pesquisar");
+		String data = scanner.next();
+		LocalDate localDate = LocalDate.parse(data, formatter);
+		
+		List<Funcionario> list = funcionarioRepository.findDataContratacaoMaior(localDate);
+		list.forEach(System.out::println);
+	}
+	
+	private void pesquisafuncionarioSalario() {
+		List<FuncionarioProjecao> list = funcionarioRepository.findFuncionarioSalario();
+		list.forEach(f -> System.out.println("Funcionario: id: " + f.getId() 
+		+ " | nome: " + f.getNome() + " | salario: " + f.getSalario()));
+	}
+	
 }
